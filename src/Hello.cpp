25648613e67980d00e42bb103ddfd72c02a039b6
siridevo <89293966+siridevo@users.noreply.github.com>
2024-06-07 14:05:38 +0530

    new4

--- src/Hello.cpp
+++ src/Hello.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
+
+// main.cpp
+#include <iostream>
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
+
 ## This is main
 ## This is another
 
